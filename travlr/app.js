96bf4547936f8072b7913f821edb735116951fed
Janiceaine <janiceaine6@outlook.com>
2025-01-26 15:35:20 -0600

    Added Module3 files

--- travlr/app.js
+++ travlr/app.js
@@ -3,21 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var handlebars = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-var handlebars = require('hbs');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
-app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
