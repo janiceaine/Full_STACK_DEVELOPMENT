a551c62d3f6e1c66435246c9efba29ec8feddfb4
Janiceaine <janiceaine6@outlook.com>
2025-01-26 17:09:04 -0600

    Updated the /travel from travel.html in all areas

--- travlr/app_server/controllers/main.js
+++ travlr/app_server/controllers/main.js
@@ -1,5 +1,6 @@
 /* GET Homepage */
 const index = (req, res) => {
+    console.log("I came here first");
     res.render('index', { title: "Travlr Getaways" });
 };
 
--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -4,5 +4,4 @@ var controller = require('../controllers/travel');
 
 /* GET travel page */
 router.get('/', controller.travel);
-
 module.exports = router;
